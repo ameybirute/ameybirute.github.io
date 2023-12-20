13308cbe93795b54fe05e8ae42e51cff7239a295
Amey <ameybirute3@gmail.com>
2023-12-20 13:10:56 +0530

    code not working fix

--- hackerman/script.js
+++ hackerman/script.js
@@ -1,31 +1,34 @@
-const arr=[
-    "Initializing Tool",
-    "Waiting for response",
-    "Few seconds more...",
-    "Searching for user",
-    "User found",
-    "Finding vulnerabilities",
-    "Trying to guess password",
-    "Attempt failed",
-    "Expanding the search list",
-    "Attempt failed(2)",
-    "Trying brute force attack",
-    "Wait..",
-    "ATTEMPT SUCCEED!!!"
+let messages=[
+  "Initializing Tool",
+  "Waiting for response",
+  "Few seconds more...",
+  "Searching for user",
+  "User found",
+  "Finding vulnerabilities",
+  "Trying to guess password",
+  "Attempt failed",
+  "Expanding the search list",
+  "Attempt failed(2)",
+  "Trying brute force attack",
+  "Wait..",
+  "ATTEMPT SUCCEED!!!"
 ]
 
-const sleep=async(n)=>{
-  return new Promise((resolve)=>{
-      setTimeout(function(){
-      resolve()
-  },(n*1000))  
-  })
+const sleep = async (seconds) => {
+return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(true) }, seconds * 1000)
+})
 }
 
-(async()=>{
-for(let i=0;i<arr.length;i++){
-  await sleep(2)
-  console.log(arr[i])
+const showHack = async (message) => {
+await sleep(2)
+text.innerHTML = text.innerHTML + message + "<br>"
 }
-})()
 
+const myfn=async()=>{
+document.getElementById("container").style.display="block"
+document.getElementById("buttondiv").style.display="none"
+for (let i = 0; i < messages.length; i++) {
+        await showHack(messages[i])
+}
+}  
